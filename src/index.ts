00b1b472849d1216b80c441da7b932c9ae4c913b
Stefan Holmberg <stefan.holmberg@systementor.se>
2024-02-29 16:54:49 +0100

    sql

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,26 @@ import express, { Express, Request, Response } from "express";
 import  { Message, getAllMessages } from "./models/message";
 import dotenv from "dotenv";
 import { findAll, createIfNotExists,seedProducts } from "./database/product";
- 
+import ejs from "ejs";
 dotenv.config();
  
 const app: Express = express();
 const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs')
+ 
  
 app.get("/test", (req: Request, res: Response) => {
     res.json(getAllMessages());
   });
+
+
+  app.get("/products", async (req: Request, res: Response) => {
+    const allProducts = await findAll()
+    res.render('pages/products',{
+      allProducts
+  });
+});
+
 
 
   app.get("/test2", async (req: Request, res: Response) => {
